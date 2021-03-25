18be819d0f4d3a44a801fdaa94498845d8c0b42f
usagiga <usagiga@usagiga.me>
2021-03-25 21:13:12 +0900

    Create blank page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import {FC} from "react";
+import Head from "next/head";
+
+const Index: FC = () => (
+    <>
+        <Head>
+            <title>Hello, World!</title>
+        </Head>
+
+        <p>Hello, World!</p>
+    </>
+)
+
+export default Index;
