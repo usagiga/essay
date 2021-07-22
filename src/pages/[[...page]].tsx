56e4b1e12cd9fd77345520654a81ffd038338c13
usagiga <usagiga@usagiga.me>
2021-07-22 14:38:23 +0900

    fix: split components and CSS into container layout

--- src/pages/[[...page]].tsx
+++ src/pages/[[...page]].tsx
@@ -1,71 +1,56 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { FC } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { post } from '../utils/esa/types/post';
 import { client } from '../utils/esa/factory';
+import style from './[[...page]].module.scss';
 
-const ArticleList: FC<{ articles: post[] }> = ({ articles }) => {
-  const title = 'BlogTitle';
-  const logoPath = process.env.ESSAY_LOGO_PATH ?? '';
+const ArticleList: FC<{ articles: post[] }> = ({ articles }) => (
+  <>
+    <Head>
+      <title>Hello, World!</title>
+    </Head>
 
-  return (
-    <>
-      <Head>
-        <title>Hello, World!</title>
-      </Head>
+    <div className={style.container}>
+      {articles?.map((article) => (
+        <article key={article.number}>
+          <time dateTime={article.created_at}>
+            {(() => {
+              const date = new Date(article.created_at);
+              const padLeft = (num: number, length: number): string => {
+                const str = `${num}`.padStart(length, '0');
 
-      <div className="page">
-        <header>
-          {logoPath !== '' && (
-            <div className="logo">
-              <Image src={logoPath} alt="Logo" width={64} height={64} />
-            </div>
+                return str.slice(-length);
+              };
+              const y = date.getFullYear();
+              const m = padLeft(date.getMonth() + 1, 2);
+              const d = padLeft(date.getDate(), 2);
+
+              return `${y}/${m}/${d}`;
+            })()}
+          </time>
+          <header>
+            <h2>
+              <Link href={`/page/${article.number}`}>
+                <a>{article.name}</a>
+              </Link>
+            </h2>
+          </header>
+          {article.tags && (
+            <ul className={style.tags}>
+              {article.tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
           )}
-          <h1>{title}</h1>
-        </header>
-        <main>
-          {articles?.map((article) => (
-            <article key={article.number}>
-              <time dateTime={article.created_at}>
-                {(() => {
-                  const date = new Date(article.created_at);
-                  const padLeft = (num: number, length: number): string => {
-                    const str = `${num}`.padStart(length, '0');
-
-                    return str.slice(-length);
-                  };
-                  const y = date.getFullYear();
-                  const m = padLeft(date.getMonth() + 1, 2);
-                  const d = padLeft(date.getDate(), 2);
-
-                  return `${y}/${m}/${d}`;
-                })()}
-              </time>
-              <header>
-                <h2>
-                  <Link href={`/page/${article.number}`}>
-                    <a>{article.name}</a>
-                  </Link>
-                </h2>
-              </header>
-              {article.tags && (
-                <ul className="tags">
-                  {article.tags.map((tag) => (
-                    <li key={tag}>{tag}</li>
-                  ))}
-                </ul>
-              )}
-            </article>
-          ))}
-          {/* TODO : Put pager here */}
-        </main>
-      </div>
-    </>
-  );
-};
+        </article>
+      ))}
+      {/* TODO : Put pager here */}
+    </div>
+  </>
+);
 
 export const getStaticProps: GetStaticProps = async (_) => {
   const articles = await client.getPosts();
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import 'modern-css-reset/src/reset.css';
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import BaseLayout from '../layouts/base';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <BaseLayout>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </BaseLayout>
+  );
 }
 
 export default MyApp;
